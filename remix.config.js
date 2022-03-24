2287fc07f456546e79de10e1a83c7efb5af93f05
Denis <fixprogram@mail.ru>
2022-03-24 17:09:32 +0200

    Remix config has changed for netlify

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,9 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "netlify/functions/server/build",
+  devServerPort: 8888,
 };
