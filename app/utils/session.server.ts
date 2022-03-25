d4b885e996e8bec345132ae03af220602994e3a5
Denis <fixprogram@mail.ru>
2022-03-25 11:54:45 +0200

    Added cookie secrets for testing purposes

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -40,7 +40,7 @@ const storage = createCookieSessionStorage({
     // but that doesn't work on localhost for Safari
     // https://web.dev/when-to-use-local-https/
     secure: process.env.NODE_ENV === "production",
-    secrets: [sessionSecret],
+    secrets: ["feriy4gf"],
     sameSite: "lax",
     path: "/",
     maxAge: 60 * 60 * 24 * 30,
