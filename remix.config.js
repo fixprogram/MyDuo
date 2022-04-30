6ea300a2cdbfe03cea15cbb33c875c782694e55b
Denis <fixprogram@mail.ru>
2022-04-30 18:40:50 +0300

    netlify test

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,10 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-// module.exports = {
-//   appDirectory: "app",
-//   browserBuildDirectory: "public/build",
-//   publicPath: "/build/",
-//   serverBuildDirectory: "netlify/functions/server/build",
-//   devServerPort: 8888,
-// };
 module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildPath: "build",
-  serverBuildTarget: "vercel",
+  serverBuildDirectory: "netlify/functions/server/build",
   devServerPort: 8888,
 };
